8c30cd725a208c5c13c9c13352d92541114be2ab
Ian Bantoto <lcbantoto@gmail.com>
2017-09-24 17:14:22 -0400

    check bugfix

--- app.js
+++ app.js
@@ -129,7 +129,15 @@ function sendMessage(event) {
 
               if(data.length > 1) {
 
+                var key = data[0].name;
+                var index = data[0].mentions[0].text.beginOffset;
+                var length = key.length;
+                console.log(length);
+                var newText = articlesData[0];
                 var blank='_______';
+                newText = articlesData[0].substring(0,index) + blank + articlesData[0].substring(index+length);
+
+                /*var blank='_______';
                 answer = data[0].name;
                 var newText = answer + ':::';
                 console.log(answer.length);
@@ -141,7 +149,7 @@ function sendMessage(event) {
                   if(i < arrAns.length - 1){
                     newText += blank;
                   }
-                }
+                }*/
 
                 // Send request to Messenger
                 request({
